58accc148f0d7e5095532e686ea9274f68f1bfe9
cazamudio90 <36246888+cazamudio90@users.noreply.github.com>
2020-08-31 20:35:21 -0300

    Add files via upload

---
+++ Ejercicio_11/Validacion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_11
+{
+    class Validacion
+    {
+        public static bool Validar(int valor, int min, int max)
+        {
+            int aux_valor;
+            string resp = valor.ToString();
+            if (int.TryParse(resp, out aux_valor))
+            {
+
+            }
+        }
+    }
+}
---
+++ Gui dce Ejercicios/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gui_dce_Ejercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 1");
+
+            string Numeros;
+            int aux_number;
+            int v_menor = 0;
+            int v_Mayor = 0;
+            int v_acumulador = 0;
+
+            Console.WriteLine("Ingrese 5 numeros");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Numeros = Console.ReadLine();
+              if ( int.TryParse(Numeros, out aux_number))
+                {
+                    if (i == 0)
+                    {
+                        v_menor = aux_number;
+                        v_Mayor = aux_number;
+                    }
+                    else
+                    {
+                        if (aux_number > v_Mayor)
+                        {
+                            v_Mayor = aux_number;
+                        }else if (aux_number < v_menor)
+                        {
+                            v_menor = aux_number;
+                        }
+                    }
+                    v_acumulador = v_acumulador + aux_number;
+                }
+            }
+
+            Console.WriteLine("Valor Minimo: {0}",v_menor);
+            Console.WriteLine("Valor Maximo: {0}", v_Mayor);
+            Console.WriteLine("Promedio: {0}", (v_acumulador/ 5));
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
