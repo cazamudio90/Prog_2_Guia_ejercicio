59ec43f2f663644acedd7e7d1ab0c03019dde6b5
cazamudio90 <36246888+cazamudio90@users.noreply.github.com>
2020-09-01 01:43:08 -0300

    Add files via upload

---
+++ Ejercicio_12/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resp;
+            int aux_num;
+            int v_acuulador = 0;
+            char resp_2;
+
+            
+            do
+            {
+                Console.WriteLine("Ingrse Numero Entrero");
+                resp = Console.ReadLine();
+                if (int.TryParse(resp, out aux_num))
+                {
+                    v_acuulador += aux_num;
+                }
+
+                Console.WriteLine("Desea Continuar (S/N)");
+                if (char.TryParse(Console.ReadLine(), out resp_2))
+                {
+
+                }
+
+            } while (ValidarRespuesta.ValidaS_N(resp_2));
+
+            Console.WriteLine("Total {0}",v_acuulador);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio_16/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno_1 = new Alumno();
+            Alumno alumno_2 = new Alumno();
+            Alumno alumno_3 = new Alumno();
+
+            alumno_1.apellido = "Perez";
+            alumno_1.nombre = "Juan";
+            alumno_1.legajo = 123;
+            alumno_2.apellido = "Rodriguez";
+            alumno_2.nombre = "Pedro";
+            alumno_2.legajo = 456;
+            alumno_3.apellido = "Morales";
+            alumno_3.nombre = "Micaela";
+            alumno_3.legajo = 789;
+
+            Console.WriteLine("Ingrese Primera Nota y Segunda Nota del primer alumno");
+
+            string nota1 = Console.ReadLine();
+            string nota2 = Console.ReadLine();
+            byte auxNota1;
+            byte auxNota2;
+
+            if (byte.TryParse(nota1, out auxNota1) && byte.TryParse(nota2, out auxNota2))
+            {
+                alumno_1.Estudiar((byte)auxNota1, (byte)auxNota2);
+            }
+
+            Console.WriteLine("Ingrese Primera Nota y Segunda Nota del segundo alumno");
+
+            nota1 = Console.ReadLine();
+            nota2 = Console.ReadLine();
+
+            if (byte.TryParse(nota1, out auxNota1) && byte.TryParse(nota2, out auxNota2))
+            {
+                alumno_2.Estudiar((byte)auxNota1, (byte)auxNota2);
+            }
+
+            Console.WriteLine("Ingrese Primera Nota y Segunda Nota del tercer alumno");
+
+            nota1 = Console.ReadLine();
+            nota2 = Console.ReadLine();
+
+            if (byte.TryParse(nota1, out auxNota1) && byte.TryParse(nota2, out auxNota2))
+            {
+                alumno_3.Estudiar((byte)auxNota1, (byte)auxNota2);
+            }
+
+            alumno_1.CalcularFinal();
+            alumno_2.CalcularFinal();
+            alumno_3.CalcularFinal();
+
+            Console.WriteLine("Primer alumno primera nota, segunda nota, nota final");
+            alumno_1.Mostrar();
+            Console.WriteLine("Segundo alumno primera nota, segunda nota, nota final");
+            alumno_2.Mostrar();
+            Console.WriteLine("Tercer alumno primera nota, segunda nota, nota final");
+            alumno_3.Mostrar();
+
+
+            Console.ReadKey();
+        }
+    }
+}
